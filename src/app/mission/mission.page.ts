70e799f468289f75c06779e993795aa41c15020f
mino9421 <mino9421@gmail.com>
2022-04-13 18:20:47 -0400

    added map

--- src/app/mission/mission.page.ts
+++ src/app/mission/mission.page.ts
@@ -1,8 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { NavController } from '@ionic/angular';
 import { Mission } from '../models/mission';
 import { DatabaseService } from '../shared/database.service';
+
+import { Geolocation } from '@capacitor/geolocation';
+
+declare let google: any;
 
 @Component({
   selector: 'app-mission',
@@ -10,8 +14,11 @@ import { DatabaseService } from '../shared/database.service';
   styleUrls: ['./mission.page.scss'],
 })
 export class MissionPage implements OnInit {
+
+  @ViewChild('map', {read: ElementRef, static: false}) mapRef: ElementRef;
   mission: Mission;
   loading = true;
+  map: any;
 
   constructor(private navController: NavController,
               private actRoute: ActivatedRoute,
@@ -32,4 +39,47 @@ export class MissionPage implements OnInit {
     this.goBack();
   }
 
+
+  // map methods
+  async ionViewDidEnter() {
+    this.getCurrentLocation();
+  }
+
+  showMap(loc: any) {
+    const location = new google.maps.LatLng(this.mission.location.latitude, this.mission.location.longitude);
+    const options = {
+      center: location,
+      zoom: 15,
+      disableDefaultUI: true
+    };
+    this.map = new google.maps.Map(this.mapRef.nativeElement, options);
+    this.addMarkerToMap(loc);
+  }
+
+  async getCurrentLocation() {
+    const coordinates = await Geolocation.getCurrentPosition();
+    this.showMap(coordinates);
+    console.log(coordinates);
+  }
+
+  addMarkerToMap(loc: any) {
+    let position = new google.maps.LatLng(this.mission.location.latitude, this.mission.location.longitude);
+    let mapMarker = new google.maps.Marker({
+      position,
+      title: this.mission.name,
+      latitude: this.mission.location.longitude,
+      longitude: this.mission.location.longitude,
+      task: this.mission.tasks,
+      tags: this.mission.tags
+    });
+    mapMarker.setMap(this.map);
+    // this.addInfoWindowToMarker(mapMarker);
+  }
+
+  customNavigate() {
+    console.log('navigate button clicked');
+    // navigate using google maps
+    // eslint-disable-next-line max-len
+    window.open('https://www.google.com/maps/dir?api=1&destination=' + this.mission.location.latitude + ',' + this.mission.location.longitude);
+  }
 }
